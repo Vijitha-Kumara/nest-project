ac88a6e0e9a94a46e78ee80eea663ff0cdc95e16
Vijitha Kumara Ranamuka <vijitha.kumara.wu@gmail.com>
2026-04-21 17:01:01 +0530

    feat: Move user schema to new directory and update related logic

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from './schemas/user.schema';
+import { User } from '../schemas/user.schema';
 
 @Injectable()
 export class UsersService {
@@ -18,3 +18,4 @@ export class UsersService {
     return this.userModel.find();
   }
 }
+
